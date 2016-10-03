5abd6859efff94075710eb0a82a05842f88713ce
Leonardo Lorbicki <leonardosaints@gmail.com>
2016-10-03 01:49:27 -0300

    Adicionado funcao de login, autoload de tweets e algumas melhorias de performance

--- www/js/app.js
+++ www/js/app.js
@@ -1,30 +1,54 @@
-function getJson(){
-  return $.getJSON("tweets.json");
-}
-var jsonFile = getJson();
+var jsonSize = 0;
 
 function loadTweets(){
-  $.each( jsonFile.responseJSON.reverse() , function(key, val){
-    $(".tweets").append('<div class="individual-tweets"><span class="owner">'+val.owner+' Tweetou:</span><p class="content">'+val.content+'</p></div>');
+  $.getJSON("tweets.json" , function(data){
+    if (data.length !== jsonSize){
+      jsonSize = data.length;
+      $(".tweets").empty();
+      $.each( data.reverse() , function(key, val){
+          $(".tweets").append('<div class="individual-tweets"><span class="owner">'+val.owner+'</span><span> Tweetou:</span><p class="content">'+val.content+'</p></div>');
+      });
+    }
   });
 }
+
+function addEdit(){
+  $.each( $(".individual-tweets .owner") , function(k, v){
+    if (v.innerText == localStorage['owner']){
+      // v.siblings("p").className += " editable";
+      console.log(v.parent());
+    }
+  });
+}
+
+function editInput(element){
+  $(element).replaceWith(function(){
+      return $('<input/>', {
+                'class': this.className,
+                 content: this.value
+             })
+  })
+};
 
 $.post = function(url, data, callback, type){
   var newTweet = new Object();
-  newTweet.owner = "leo";
+  newTweet.owner = localStorage['owner'];
   newTweet.content = $("#tweetContent").val();
 
-  jsonFile.responseJSON.push(newTweet);
+  $.getJSON("tweets.json" , function(data){
+    data.push(newTweet);
+    $.ajax({
+      url: url,
+      type: 'POST',
+      contentType:'json',
+      data: JSON.stringify(data),
+      dataType: 'text'
+    });
 
-  $.ajax({
-    url: url,
-    type: 'POST',
-    contentType:'json',
-    data: JSON.stringify(jsonFile.responseJSON),
-    dataType: 'text'
-  });
+   });
 
-  $(".tweets").prepend('<div class="individual-tweets"><span class="owner">'+newTweet.owner+' Tweetou:</span><p class="content">'+newTweet.content+'</p></div>');
+  $(".tweets").prepend('<div class="individual-tweets"><span class="owner">'+newTweet.owner+'</span><span> Tweetou:</span><p class="content">'+newTweet.content+'</p></div>');
+
 }
 
 $.edit = function(url, data, callback, type){
@@ -45,3 +69,4 @@ $.delete = function(url, data, callback, type){
     contentType: type
   });
 }
+
